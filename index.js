07ec6e26a6df9bed8add8c5cfc8c35f33b88886d
cobbdzon <clarkjacob07@gmail.com>
2024-01-19 11:22:57 +0800

    line scroll animation

--- index.js
+++ index.js
@@ -15,8 +15,21 @@ function contentLoaded () {
     
     header_glow.style.opacity = 1
 
-    const genmath_section = document.getElementById("genmath")
-    const genmath_line = genmath_section.getElementsByClassName("line")[0]
+    document.body.querySelectorAll("section").forEach((section) => {
+        const section_line = section.getElementsByClassName("line")[0]
+        const section_card = section.getElementsByClassName("card")[0]
+        if (section_line && section_card) {
+            gsap.set(section_line, {scaleY: 0})
+            gsap.to(section_line, {
+                scrollTrigger: {
+                    trigger: section_card,
+                    toggleActions: "restart none none none",
+                    scrub: 1,
+                },
+                scaleY: 1,
+            })
+        }
+    })
 
     setTimeout(() => {
         const loading = document.getElementById("loading")
@@ -27,7 +40,7 @@ function contentLoaded () {
         ScrollReveal().reveal(header_title_sup, {delay: 500, distance: '50px'})
         ScrollReveal().reveal(header_tagline, {delay: 750, distance: '50px'})
         ScrollReveal().reveal(header_glow, {delay: 1250})
-        ScrollReveal().reveal(document.getElementsByClassName("card"), {delay: 250, distance: '50px'})
+        ScrollReveal().reveal(document.getElementsByClassName("card"), {delay: 500, distance: '50px', reset: true})
     }, 250)
 }
 
