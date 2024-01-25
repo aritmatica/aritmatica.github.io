daa1299983a4fa04e09652e585f731175fc06e21
cobbdzon <clarkjacob07@gmail.com>
2024-01-26 06:36:07 +0800

    modules nav responsive fix

--- modules/coursePage.js
+++ modules/coursePage.js
@@ -4,25 +4,32 @@ function animateModulesNav() {
     const open_modules_nav = document.getElementById("open-modules-nav")
     const modules_nav = document.getElementById("modules-nav")
 
+    function isDesktopDevice() {
+        return document.body.clientWidth > BREAK_POINT
+    }
+
     function updateModulesNav() {
-        if (open_modules_nav.checked === true) {
-            gsap.to(modules_nav, {
-                translateY: 0,
-            })
-        } else {
-            gsap.to(modules_nav, {
-                translateY: "-100%",
-            })
+        if (isDesktopDevice() === false) {
+            if (open_modules_nav.checked === true) {
+                gsap.to(modules_nav, {
+                    translateY: 0,
+                })
+            } else {
+                gsap.to(modules_nav, {
+                    translateY: "-100%",
+                })
+            }
         }
     }
 
     open_modules_nav.addEventListener("click", updateModulesNav)
 
     window.addEventListener("resize", () => {
-        open_modules_nav.checked = false
-        gsap.set(open_modules_nav, {
-            translateY: 0,
-        })
+        if (isDesktopDevice()) {
+            console.log("farted")
+            open_modules_nav.checked = false
+            modules_nav.style = ""
+        }
     })
 }
 
