dc1693559c89c75e54148363f00014f9b059a824
cobbdzon <clarkjacob07@gmail.com>
2024-01-25 15:36:18 +0800

    home svg design ui

---
+++ index.js
@@ -0,0 +1,40 @@
+function genmathLog() {
+    const genmath_log = document.getElementById("genmath-log")
+    gsap.to(genmath_log, {
+        scrollTrigger: {
+            trigger: genmath_log,
+            scrub: 1,
+        },
+        opacity: 0.8,
+    })
+}
+
+function precalcCirle() {
+    const precalc_circle = document.getElementById("precalc-circle")
+    gsap.to(precalc_circle, {
+        scrollTrigger: {
+            trigger: precalc_circle,
+            scrub: 1,
+        },
+        opacity: 0.8,
+    })
+
+    gsap.to(precalc_circle, {
+        rotate: -360,
+        repeat: -1,
+        duration: 60,
+        ease: "none",
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    genmathLog()
+    precalcCirle()
+    renderMathInElement(document.body, {
+        delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\[", right: "\\]", display: true }
+        ]
+    })
+})
