6ecdfb94f67d1f7156ef9693f117f33b50167b67
cobbdzon <clarkjacob07@gmail.com>
2024-01-26 10:52:26 +0800

    scroll animations readded

--- index.js
+++ index.js
@@ -41,8 +41,53 @@ function exercisesShape() {
     })
 }
 
+function scrollAnimations() {
+    const header = document.querySelector("header")
+
+    const header_main = header.querySelector("main")
+    const header_title = header_main.querySelector("h1")
+    const header_title_sup = header_title.querySelector("sup")
+    const header_tagline = header_main.querySelector("span")
+    const header_glow = header_main.getElementsByClassName("glow")[0]
+
+    header_glow.style.opacity = 1
+
+    const exercises_section = document.getElementById("exercises")
+    const exercises_line = exercises_section.getElementsByClassName("line")[0]
+
+    const join_section = document.getElementById("join")
+    const join_line = join_section.getElementsByClassName("line")[0]
+
+    const sections = document.body.querySelectorAll("section")
+    for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        const section_line = section.getElementsByClassName("line")[0]
+        const section_card = section.getElementsByClassName("card")[0]
+        if (section_line && section_card) {
+            gsap.set(section_line, {scaleY: 0})
+            gsap.to(section_line, {
+                scrollTrigger: {
+                    trigger: section_line,
+                    start: "top-=250px top",
+                    end: "+=256px",
+                    toggleActions: "restart none none none",
+                    scrub: 1,
+                },
+                scaleY: 1,
+            })
+        }
+    }
+
+    ScrollReveal().reveal(header_title, {delay: 250, distance: '50px'})
+    ScrollReveal().reveal(header_title_sup, {delay: 500, distance: '50px'})
+    ScrollReveal().reveal(header_tagline, {delay: 750, distance: '50px'})
+    ScrollReveal().reveal(header_glow, {delay: 1250})
+    ScrollReveal().reveal(document.getElementsByClassName("card"), {delay: 100, reset: true})
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     genmathLog()
     precalcCirle()
     exercisesShape()
+    scrollAnimations()
 })
