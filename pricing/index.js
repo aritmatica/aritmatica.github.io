051077356d521ba60cde0d1c7325a1317afe7a98
cobbdzon <clarkjacob07@gmail.com>
2024-01-27 12:36:02 +0800

    pricing free trial error

---
+++ pricing/index.js
@@ -0,0 +1,52 @@
+const searchParams = new URLSearchParams(window.location.search);
+
+function removeURLParameter(url, parameter) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts = url.split('?');   
+    if (urlparts.length >= 2) {
+
+        var prefix = encodeURIComponent(parameter) + '=';
+        var pars = urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i = pars.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+                pars.splice(i, 1);
+            }
+        }
+
+        return urlparts[0] + (pars.length > 0 ? '?' + pars.join('&') : '');
+    }
+    return url;
+}
+
+function handleErrorPrompt() {
+    const error_prompt = document.getElementById("error-prompt")
+    const error_continue = document.getElementById("error-continue")
+
+    var clicked = false
+
+    error_continue.onclick = () => {
+        if (clicked != true) {
+            clicked = true
+            error_prompt.style.opacity = 0
+            setTimeout(() => {
+                searchParams.delete("error")
+                document.body.removeAttribute("show-error")
+                if (window.history.replaceState) {
+                    window.history.replaceState(null, null, removeURLParameter(window.location.href, "error"));
+                } else {
+                    window.location.href = removeURLParameter(window.location.href, "error")
+                }
+            }, 250);
+        }
+    }
+}
+
+const errorType = searchParams.get("error")
+
+if (errorType != null) {
+    handleErrorPrompt()
+    document.body.setAttribute("show-error", errorType)
+}
